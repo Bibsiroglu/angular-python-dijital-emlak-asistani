1d14a9cc70010c1b0656af9e687e97a98a0219ce
Bibsiroglu <buseibsiroglu@gmail.comm>
2025-10-27 14:44:20 +0300

    MulkListesiComponent'teki Standalone ve Template hataları giderildi

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MulkListesiComponent } from './components/mulk-listesi/mulk-listesi';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MulkListesiComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('emlak-ui');
+  protected readonly title = signal('emlak_asistani');
 }
